4906e027c0281553a18edae9a915acf1e693ae7c
Ian Dees <ian.dees@gmail.com>
2013-05-24 20:58:45 -0500

    A start with a fab file.

---
+++ fabfile.py
@@ -0,0 +1,39 @@
+from fabric.api import *
+from fabric.contrib.files import *
+from fabric.colors import red
+
+
+def deploy(branch='master'):
+    "Deploy the specified branch to the remote host."
+
+    code_dir = 'django_app'
+    virtualenv_name = 'django_venv'
+
+    # Install required packages
+    sudo('apt-get install -y git')
+
+    # Install up to virtualenv
+    sudo('apt-get install -y python-setuptools')
+    sudo('easy_install pip')
+    sudo('pip install virtualenv')
+
+    # Create virtualenv and add our django app to its PYTHONPATH
+    run('virtualenv --no-site-packages %s' % virtualenv_name)
+    run('rm -f %s/lib/python2.7/site-packages/censusreporter.pth' % virtualenv_name)
+    append('%s/lib/python2.7/site-packages/censusreporter.pth' % virtualenv_name, '/home/ubuntu/%s/censusreporter' % code_dir)
+    append('%s/lib/python2.7/site-packages/censusreporter.pth' % virtualenv_name, '/home/ubuntu/%s/censusreporter/apps' % code_dir)
+
+    with settings(warn_only=True):
+        if run('test -d %s' % code_dir).failed:
+            print(red('Cloning fresh repo.'))
+            run('git clone git://github.com/censusreporter/censusreporter.git %s' % code_dir)
+
+    with cd(code_dir):
+        print(red('Pulling %s from GitHub' % branch))
+        run('git pull origin %s' % branch)
+
+        # Install pip requirements
+        run('source /home/ubuntu/%s/bin/activate && pip install -r requirements.txt' % virtualenv_name)
+
+        # Run the server
+        run('source /home/ubuntu/%s/bin/activate && python manage.py runserver' % virtualenv_name)
