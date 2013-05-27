850002509335471a5a9b9ca3365528eac99e21bd
Ryan Pitts <ryan.a.pitts@gmail.com>
2013-05-27 09:59:44 -0700

    add base comparison list

--- censusreporter/apps/census/urls.py
+++ censusreporter/apps/census/urls.py
@@ -20,16 +20,24 @@ urlpatterns = patterns('',
         regex   = '^(?P<geography_type>%s)/(?P<geography_id>[-\w]+)/$' % geography_type_options,
         view    = TemplateView.as_view(template_name='placeholder.html'),
         kwargs  = {},
-        name    = 'geography_profile',
+        name    = 'geography_detail',
     ),
 
     # e.g. /compare/states/washington/counties/
     url(
-        regex   = '^compare/(?P<parent_type>%s)/(?P<geography_id>[-\w]+)/(?P<descendant_type>%s)/(?P<comparison_type>%s)/$' % (geography_type_options, geography_type_options, comparison_types),
+        regex   = '^compare/(?P<parent_type>%s)/(?P<geography_id>[-\w]+)/(?P<descendant_type>%s)/$' % (geography_type_options, geography_type_options),
         view    = ComparisonView.as_view(),
         kwargs  = {},
         name    = 'geography_comparison',
     ),
+    # e.g. /compare/states/washington/counties/map/
+    url(
+        regex   = '^compare/(?P<parent_type>%s)/(?P<geography_id>[-\w]+)/(?P<descendant_type>%s)/(?P<comparison_type>%s)/$' % (geography_type_options, geography_type_options, comparison_types),
+        view    = ComparisonView.as_view(),
+        kwargs  = {},
+        name    = 'geography_comparison_detail',
+    ),
+    # e.g. internally transform API json into needed format
     url(
         regex   = '^compare/(?P<parent_type>%s)/(?P<geography_id>[-\w]+)/(?P<descendant_type>%s)/json/$' % (geography_type_options, geography_type_options),
         view    = ComparisonJsonMaker.as_view(),
--- censusreporter/apps/census/views.py
+++ censusreporter/apps/census/views.py
@@ -61,13 +61,17 @@ class ComparisonJsonMaker(View):
         return self.render_json_to_response(data_groups)
 
 class ComparisonView(TemplateView):
+    template_name = 'comparison.html'
+    
     def get_context_data(self, *args, **kwargs):
         parent_type = self.kwargs['parent_type']
         geography_id = self.kwargs['geography_id']
         descendant_type = self.kwargs['descendant_type']
-        comparison_type = self.kwargs['comparison_type']
-        
-        self.template_name = 'comparison_%s.html' % comparison_type
+
+        comparison_type = self.kwargs.get('comparison_type', None)
+        if comparison_type:
+            self.template_name = 'comparison_%s.html' % comparison_type
+
         page_context = {
             'parent_type': parent_type,
             'geography_id': geography_id,
@@ -79,85 +83,92 @@ class ComparisonView(TemplateView):
         API_ENDPOINT = API_ENDPOINTS[geography_id]
         r = requests.get(API_ENDPOINT)
         data = simplejson.loads(r.text, object_pairs_hook=collections.OrderedDict)
+        
+        # add a list of all the descendants being compared
+        descendant_list = sorted([(geo['geography']['geoid'], geo['geography']['name']) for geo in data])
+        page_context.update({
+            'descendant_list': descendant_list,
+        })
 
         if comparison_type == 'table':
-            geo_values = []
-            for geo in data:
-                name = geo['geography']['name']
-                geoID = geo['geography']['geoid']
-                total_population = geo['population']['total']
-                geo_item = {
-                    'name': name,
-                    'geoID': geoID,
-                    'total_population': total_population,
-                    'values': collections.OrderedDict(),
-                }
-                for group, values in geo['population']['gender'].items():
-                    geo_item['values'].update({
-                        group: {
-                            'male': values['male'],
-                            'male_pct': round((values['male'] / values['total'])*100,1),
-                            'female': values['female'],
-                            'female_pct': round((values['female'] / values['total'])*100,1),
-                            'total': values['total'],
-                            'total_pct': round((values['total'] / total_population)*100,1),
-                        }
-                    })
-                geo_values.append(geo_item)
-
-            page_context.update({
-                'geo_values': geo_values,
-            })
-
-
-        elif comparison_type == 'distribution' or 'map':
-            coal_charts = collections.OrderedDict()
-            for geo in data:
-                name = geo['geography']['name']
-                geoID = geo['geography']['geoid']
-                total_population = geo['population']['total']
-                for group, values in geo['population']['gender'].items():
-                    if not group in coal_charts:
-                        coal_charts[group] = {
-                            'group_baselines': {},
-                            'group_values': {},
-                        }
-                    coal_charts[group]['group_values'].update({
-                        geoID: {
-                            'name': name,
-                            'male': values['male'],
-                            'male_pct': round((values['male'] / values['total'])*100,1),
-                            'female': values['female'],
-                            'female_pct': round((values['female'] / values['total'])*100,1),
-                            'total': values['total'],
-                            'total_pct': round((values['total'] / total_population)*100,1),
-                        }
-                    })
-
-            for chart, chart_values in coal_charts.items():
-                for field in ['total', 'total_pct']:
-                    values_list = [value[field] for geo, value in chart_values['group_values'].items()]
-                    max_value = max(values_list)
-                    min_value = min(values_list)
-                    domain_range = max_value - min_value
-                    median_value = int(median(values_list))
-                    median_percent_of_range = ((median_value - min_value) / domain_range)*100
-                    chart_values['group_baselines']['max_value_%s' % field] = max_value
-                    chart_values['group_baselines']['min_value_%s' % field] = min_value
-                    chart_values['group_baselines']['domain_range_%s' % field] = domain_range
-                    chart_values['group_baselines']['median_value_%s' % field] = median_value
-                    chart_values['group_baselines']['median_percent_of_range_%s' % field] = round(median_percent_of_range,1)
-                    for geo, value in chart_values['group_values'].items():
-                        percentage = ((value[field] - min_value) / domain_range)*100
-                        value['percent_of_range_%s' % field] = round(percentage,1)
-
-            geography_list = sorted([(geo, value['name']) for geo, value in chart_values['group_values'].items()])
-
-            page_context.update({
-                'coal_charts': coal_charts,
-                'geography_list': geography_list,
-                'api_endpoint': API_ENDPOINT,
-            })
-
+            self.add_table_values(page_context, data)
+            
+        elif comparison_type == 'distribution':
+            self.add_distribution_values(page_context, data)
+            
         return page_context
+        
+    def add_table_values(self, page_context, data):
+        geo_values = []
+        for geo in data:
+            name = geo['geography']['name']
+            geoID = geo['geography']['geoid']
+            total_population = geo['population']['total']
+            geo_item = {
+                'name': name,
+                'geoID': geoID,
+                'total_population': total_population,
+                'values': collections.OrderedDict(),
+            }
+            for group, values in geo['population']['gender'].items():
+                geo_item['values'].update({
+                    group: {
+                        'male': values['male'],
+                        'male_pct': round((values['male'] / values['total'])*100,1),
+                        'female': values['female'],
+                        'female_pct': round((values['female'] / values['total'])*100,1),
+                        'total': values['total'],
+                        'total_pct': round((values['total'] / total_population)*100,1),
+                    }
+                })
+            geo_values.append(geo_item)
+
+        page_context.update({
+            'geo_values': geo_values,
+        })
+
+    def add_distribution_values(self, page_context, data):
+        distribution_groups = collections.OrderedDict()
+        for geo in data:
+            name = geo['geography']['name']
+            geoID = geo['geography']['geoid']
+            total_population = geo['population']['total']
+            for group, values in geo['population']['gender'].items():
+                if not group in distribution_groups:
+                    distribution_groups[group] = {
+                        'group_baselines': {},
+                        'group_values': {},
+                    }
+                distribution_groups[group]['group_values'].update({
+                    geoID: {
+                        'name': name,
+                        'male': values['male'],
+                        'male_pct': round((values['male'] / values['total'])*100,1),
+                        'female': values['female'],
+                        'female_pct': round((values['female'] / values['total'])*100,1),
+                        'total': values['total'],
+                        'total_pct': round((values['total'] / total_population)*100,1),
+                    }
+                })
+
+        for chart, chart_values in distribution_groups.items():
+            for field in ['total', 'total_pct']:
+                values_list = [value[field] for geo, value in chart_values['group_values'].items()]
+                max_value = max(values_list)
+                min_value = min(values_list)
+                domain_range = max_value - min_value
+                median_value = int(median(values_list))
+                median_percent_of_range = ((median_value - min_value) / domain_range)*100
+                chart_values['group_baselines']['max_value_%s' % field] = max_value
+                chart_values['group_baselines']['min_value_%s' % field] = min_value
+                chart_values['group_baselines']['domain_range_%s' % field] = domain_range
+                chart_values['group_baselines']['median_value_%s' % field] = median_value
+                chart_values['group_baselines']['median_percent_of_range_%s' % field] = round(median_percent_of_range,1)
+                for geo, value in chart_values['group_values'].items():
+                    percentage = ((value[field] - min_value) / domain_range)*100
+                    value['percent_of_range_%s' % field] = round(percentage,1)
+
+        page_context.update({
+            'distribution_groups': distribution_groups,
+        })
     
