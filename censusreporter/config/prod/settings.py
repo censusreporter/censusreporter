1b8ff4837180dc8f54cd07d6815e4547a7931ac2
Ryan Pitts <ryan.a.pitts@gmail.com>
2013-06-03 12:20:17 -0700

    add ALLOWED_HOSTS to prod settings

--- censusreporter/config/prod/settings.py
+++ censusreporter/config/prod/settings.py
@@ -4,14 +4,7 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 ROOT_URLCONF = 'config.prod.urls'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
+ALLOWED_HOSTS = [
+    '174.129.183.221',
+    '.censusreporter.org',
+]
