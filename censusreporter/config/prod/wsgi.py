588fcccdfecf0079ee5c63ddbad20c0668c211a2
Ian Dees <ian.dees@gmail.com>
2013-06-11 08:47:11 -0500

    Remove the activate_this call from the wsgi.

--- censusreporter/config/prod/wsgi.py
+++ censusreporter/config/prod/wsgi.py
@@ -1,8 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-activate_this = '/home/www-data/django_venv/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.prod.settings")
 application = get_wsgi_application()
