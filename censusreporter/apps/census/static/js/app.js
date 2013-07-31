2123f85c278cc1197344f299127f8dab7d8065ab
Ryan Pitts <ryan.a.pitts@gmail.com>
2013-07-31 14:15:23 -0700

    identify sumlev name for geo search results

--- censusreporter/apps/census/static/js/app.js
+++ censusreporter/apps/census/static/js/app.js
@@ -9,6 +9,20 @@ window.log = function(){
 // make it safe to use console.log always
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
-
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
-
+var sumlevMap = {
+    "010": "nation",
+    "020": "region",
+    "030": "division",
+    "040": "state",
+    "050": "county",
+    "160": "place",
+    "250": "native area",
+    "310": "metro area",
+    "500": "congressional district",
+    "610": "state house (upper)",
+    "620": "state house (lower)",
+    "860": "ZIP code",
+    "950": "school district (elementary)",
+    "960": "school district (secondary)",
+    "970": "school district (unified)"
+}
