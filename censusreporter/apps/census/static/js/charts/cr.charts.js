e02ea8deebbfcb6124105938b083772af0e2e5d3
Ryan Pitts <ryan.a.pitts@gmail.com>
2013-08-30 15:02:41 -0700

    pie chart attack!

--- censusreporter/apps/census/static/js/charts/cr.charts.js
+++ censusreporter/apps/census/static/js/charts/cr.charts.js
@@ -47,7 +47,7 @@ function Chart(options) {
 
         chart.color = d3.scale.ordinal()
             .domain(chart.chartCategories)
-            .range(chart.colorbrewer.Greens[9].reverse());
+            .range(chart.colorbrewer.Set2);
         
         chart.arc = d3.svg.arc()
             .outerRadius(chart.settings.radius - 40)
@@ -76,63 +76,89 @@ function Chart(options) {
 
         // center label
         chart.centerLabel = chart.centerGroup.append("text")
-          .attr("class", "label-name")
-          .attr("dy", -10)
-          .attr("text-anchor", "middle")
-          .text(chart.pieData[0]['data']['name']);
+            .attr("class", "label-name")
+            .attr("dy", -8)
+            .attr("text-anchor", "middle");
+            //.text(chart.pieData[0]['data']['name']);
 
         // center value
         chart.centerValue = chart.centerGroup.append("text")
-          .attr("class", "label-value")
-          .attr("dy", 12)
-          .attr("text-anchor", "middle")
-          .text(function() {
-              return chart.pctFmt(chart.pieData[0]['data']['value']);
-          });
+            .attr("class", "label-value")
+            .attr("dy", 14)
+            .attr("text-anchor", "middle");
+            //.text(function() {
+            //    return chart.pctFmt(chart.pieData[0]['data']['value']);
+            //});
+
+        chart.arcHover = function(data) {
+            chart.arcs
+                .filter(function(d) {
+                    return d == data;
+                })
+                .classed("hovered", true);
+            chart.legendItems
+                .filter(function(d) {
+                    return d == data;
+                })
+                .classed("hovered", true);
+            
+            chart.centerLabel.text(data['data']['name']);
+            chart.centerValue.text(function() {
+                return chart.pctFmt(data['data']['value']);
+            });
+        }
+
+        chart.arcReset = function() {
+            chart.arcs
+                .classed("hovered", false);
+            chart.legendItems
+                .classed("hovered", false);
+
+            chart.centerLabel.text("");
+            chart.centerValue.text("");
+        }
 
         // add arc paths
         chart.arcs = chart.arcGroup.selectAll(".arc")
                 .data(chart.pieData)
             .enter().append("path")
+                .classed("arc", true)
                 .attr("d", chart.arc)
                 .style("fill", function(d) { return chart.color(d.data['name']); });
+                
+        chart.arcs.on("mouseover", chart.arcHover)
+            .on("mouseout", chart.arcReset);
+
         
         // add legend
         chart.legend = chart.svg.append("g")
                 .attr("class", "legend");
 
-        chart.legend.selectAll('g').data(chart.pieData)
-            .enter()
-            .append('g')
-            .each(function(d, i) {
-                var g = d3.select(this);
-                    g.append("rect")
-                        .attr("x", chart.settings.width - 100)
-                        .attr("y", i*18 + 30)
-                        .attr("width", 10)
-                        .attr("height", 10)
-                        .style("fill", function(d) { return chart.color(d.data['name']); });
+        chart.legendItems = chart.legend.selectAll('g')
+                .data(chart.pieData)
+            .enter().append('g')
+                .attr("class", "legend-item")
+                .each(function(d, i) {
+                    var g = d3.select(this);
+                        g.append("rect")
+                            .attr("x", chart.settings.width - 100)
+                            .attr("y", i*18 + 30)
+                            .attr("width", 10)
+                            .attr("height", 10)
+                            .style("fill", function(d) { return chart.color(d.data['name']); });
 
-                    g.append("text")
-                        .attr("x", chart.settings.width - 85)
-                        .attr("y", i*18 + 39)
-                        .attr("height",30)
-                        .attr("width",100)
-                        .text(d.data['name']);
-            });
+                        g.append("text")
+                            .attr("x", chart.settings.width - 85)
+                            .attr("y", i*18 + 39)
+                            .attr("height",30)
+                            .attr("width",100)
+                            .text(d.data['name']);
+                });
+
+        chart.legendItems.on("mouseover", chart.arcHover)
+            .on("mouseout", chart.arcReset);
+
             
-        chart.showLabel = function(d) {
-            chart.centerLabel.text(d['data']['name']);
-            chart.centerValue.text(function() {
-                return chart.pctFmt(d['data']['value']);
-            });
-        }
-        
-        chart.resetLabel = function() {
-            chart.centerLabel.text("");
-            chart.centerValue.text("");
-        }
-
         return chart;
     }
     
@@ -154,7 +180,23 @@ function Chart(options) {
             7: ["#edf8e9","#c7e9c0","#a1d99b","#74c476","#41ab5d","#238b45","#005a32"],
             8: ["#f7fcf5","#e5f5e0","#c7e9c0","#a1d99b","#74c476","#41ab5d","#238b45","#005a32"],
             9: ["#f7fcf5","#e5f5e0","#c7e9c0","#a1d99b","#74c476","#41ab5d","#238b45","#006d2c","#00441b"]
-        }
+        },
+        RdBu: {
+            2: ["#f7f7f7","#67a9cf"],
+            3: ["#ef8a62","#f7f7f7","#67a9cf"],
+            4: ["#ca0020","#f4a582","#92c5de","#0571b0"],
+            5: ["#ca0020","#f4a582","#f7f7f7","#92c5de","#0571b0"],
+            6: ["#b2182b","#ef8a62","#fddbc7","#d1e5f0","#67a9cf","#2166ac"],
+            7: ["#b2182b","#ef8a62","#fddbc7","#f7f7f7","#d1e5f0","#67a9cf","#2166ac"],
+            8: ["#b2182b","#d6604d","#f4a582","#fddbc7","#d1e5f0","#92c5de","#4393c3","#2166ac"],
+            9: ["#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac"],
+            10: ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"],
+            11: ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"]
+        },
+        Set1: ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'],
+        Set2: ['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#e5c494', '#b3b3b3'],
+        Set3: ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f'],
+        Accent: ['#7fc97f', '#beaed4', '#fdc086', '#ffff99', '#386cb0', '#f0027f', '#bf5b17', '#666666']
     };
 
     chart.init(options);
