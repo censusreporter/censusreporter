23f8e2c7ae93d5a441ae29e502843b2f0240ee5c
Ian Dees <ian.dees@gmail.com>
2013-09-03 15:16:49 -0500

    Show the geodata on the slippymap.

--- censusreporter/apps/census/static/js/maps.profile.js
+++ censusreporter/apps/census/static/js/maps.profile.js
@@ -1,13 +1,9 @@
-var map = L.map('slippy-map', {
+var map = L.mapbox.map('slippy-map', 'iandees.map-b7tobml9', {
     scrollWheelZoom: false,
     zoomControl: false
-}).setView([42.045416,-87.698978], 13);
+});
+map.setView([42.045416,-87.698978], 13);
 
 map.addControl(new L.Control.Zoom({
     position: 'topright'
 }));
-
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; OpenStreetMap.org',
-    maxZoom: 19
-}).addTo(map);
