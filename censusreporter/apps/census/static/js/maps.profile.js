df08ee058d09f703ff7d5fd874ed50c5ef8ca63c
Ian Dees <ian.dees@gmail.com>
2013-09-05 13:00:35 -0500

    Disable box, double-click zoom on the map.

--- censusreporter/apps/census/static/js/maps.profile.js
+++ censusreporter/apps/census/static/js/maps.profile.js
@@ -1,6 +1,9 @@
 var map = L.mapbox.map('slippy-map', 'iandees.map-b7tobml9', {
     scrollWheelZoom: false,
-    zoomControl: false
+    zoomControl: false,
+    doubleClickZoom: false,
+    boxZoom: false,
+    keyboard: false
 });
 map.setView([42.045416,-87.698978], 13);
 
