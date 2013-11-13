f2ca9092d4d9ef8ed7c92e6e5bb59067c82e6dec
Ryan Pitts <ryan.a.pitts@gmail.com>
2013-11-13 12:12:23 -0800

    massage the madlibs language

--- censusreporter/apps/census/templatetags/madlibs.py
+++ censusreporter/apps/census/templatetags/madlibs.py
@@ -5,30 +5,32 @@ from numpy import array
 register = template.Library()
 
 COMPARISON_PHRASE_MAP = {
-    "206": ["more than double", ""],
-    "195": ["about double", ""],
-    "180": ["nearly double", ""],
-    "161": ["more than 1.5 times", ""],
-    "144": ["about 1.5 times", ""],
-    "133": ["nearly 1.5 times", ""],
-    "120": ["significantly higher", "than"],
-    "109": ["higher", "than"],
-    "103": ["a little higher", "than"],
-    "98": ["about the same as", ""],
-    "94": ["a little less", "than"],
-    "86": ["about 90 percent", "of"],
-    "78": ["about 80 percent", "of"],
-    "72": ["about three-quarters", "of"],
-    "64": ["about two-thirds", "of"],
-    "56": ["about three-fifths", "of"],
-    "45": ["about half", ""],
-    "37": ["about two-fifths", "of"],
-    "30": ["about one-third", "of"],
-    "23": ["about one-quarter", "of"],
-    "17": ["about two-fifths", "of"],
-    "13": ["less than two-fifths", "of"],
-    "8": ["about 10 percent", "of"],
-    "0": ["less than 10 percent", "of"],
+    206: ["more than double", ""],
+    195: ["about double", ""],
+    180: ["nearly double", ""],
+    161: ["more than 1.5 times", ""],
+    145: ["about 1.5 times", ""],
+    135: ["about 1.4 times", ""],
+    128: ["about 1.3 times", ""],
+    122: ["about 25 percent higher", "than"],
+    115: ["about 20 percent higher", "than"],
+    107: ["about 10 percent higher", "than"],
+    103: ["a little higher", "than"],
+    98: ["about the same as", ""],
+    94: ["a little less", "than"],
+    86: ["about 90 percent", "of"],
+    78: ["about 80 percent", "of"],
+    72: ["about three-quarters", "of"],
+    64: ["about two-thirds", "of"],
+    56: ["about three-fifths", "of"],
+    45: ["about half", ""],
+    37: ["about two-fifths", "of"],
+    30: ["about one-third", "of"],
+    23: ["about one-quarter", "of"],
+    17: ["about two-fifths", "of"],
+    13: ["less than two-fifths", "of"],
+    8: ["about 10 percent", "of"],
+    0: ["less than 10 percent", "of"],
 }
 
 @register.filter
@@ -49,10 +51,10 @@ def comparison_index_phrase(value):
     index = round(float(value))
     
     # get lower boundaries for each phrase in the map
-    thresholds = array(sorted([int(k) for k,v in COMPARISON_PHRASE_MAP.iteritems()]))
+    thresholds = array(sorted([k for k,v in COMPARISON_PHRASE_MAP.iteritems()]))
 
     # get highest boundary that's less than the index value we've been passed
-    phrase_key = str(max(thresholds[thresholds<=index]))
+    phrase_key = max(thresholds[thresholds<=index])
     
     phrase_bits = COMPARISON_PHRASE_MAP[phrase_key]
     phrase = "<strong>%s</strong> %s" % (phrase_bits[0], phrase_bits[1])
