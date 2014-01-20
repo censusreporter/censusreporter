b57bb49455814d80caf7913f76268e4d38bf7a0e
Joe Germuska <joe@germuska.com>
2014-01-20 12:05:08 -0600

    flesh out some terms, and adapt glossary to reflect the fact that we aren't using Sphinx to build it.

---
+++ censusreporter/apps/census/static/js/glossary.js
@@ -0,0 +1,30 @@
+/*
+  Adapted from Michael Bauer's https://github.com/mihi-tr/sphinx-glossary-js
+  TODO: Support mousing from the term to the pop-up, which sometimes has links.
+        Dismiss with esc-, or on triggering other terms instead of on mouseout?
+*/
+function Glossary() { 
+  if (!(this instanceof Glossary)) { 
+    return new Glossary(); }
+  var instance =this;
+  }
+
+Glossary.popup=function(obj) { 
+  var keyword = $(obj).data('keyword') || obj.textContent;
+  var slug = '#term-'+keyword.toLowerCase().replace(/[^a-z0-9-]/,'-');
+  data=Glossary.data;
+  $('body').append('<div id="glossarypopup"></div>');
+  $('#glossarypopup').append($(slug,data));
+  $('#glossarypopup').append($(slug,data).next());
+  $('#glossarypopup').css('top',obj.offsetTop+20);
+  $('#glossarypopup').css('left',obj.offsetLeft);
+}
+
+Glossary.init = function (url, selector) {
+  Glossary.url=url;
+  $.get(url,function(data) {
+    Glossary.data=data })
+    
+    $(selector).attr("onmouseover","Glossary.popup(this)")
+    $(selector).attr("onmouseout","$('#glossarypopup').remove()")
+  }
