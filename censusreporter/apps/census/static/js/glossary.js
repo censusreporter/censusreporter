ae24be8c4ff6c0615f46222f04570541a16dbc02
Ryan Pitts <ryan.a.pitts@gmail.com>
2014-01-28 21:57:17 -0800

    updated glossary.js, with validation of links against available terms

--- censusreporter/apps/census/static/js/glossary.js
+++ censusreporter/apps/census/static/js/glossary.js
@@ -1,66 +1,128 @@
 /*
   Adapted from Michael Bauer's https://github.com/mihi-tr/sphinx-glossary-js
-  TODO: Support mousing from the term to the pop-up, which sometimes has links.
-        Dismiss with esc-, or on triggering other terms instead of on mouseout?
 */
-function Glossary() { 
-  if (!(this instanceof Glossary)) { 
-    return new Glossary(); }
-  var instance =this;
-  }
-
-Glossary.popup=function(obj) { 
-  var keyword = $(obj).data('keyword') || obj.textContent;
-  var slug = '#term-'+keyword.toLowerCase().replace(/[^a-z0-9-]/,'-');
-  var data=Glossary.data;
-  $('#glossarypopup').remove(); // just in case
-  $('body').append('<div id="glossarypopup"></div>');
-  $('#glossarypopup').append($(slug,data));
-  $('#glossarypopup').append($(slug,data).next());
-  $('#glossarypopup').css('top',obj.offsetTop+20);
-  $('#glossarypopup').css('left',obj.offsetLeft);
+function Glossary() {
+    if (!(this instanceof Glossary)) {
+        return new Glossary();
+    }
+    var instance = this;
 }
 
-Glossary.init = function (url, selector, watch_container_selector) {
+Glossary.init = function (url, selector, container_selector) {
+    Glossary.terms = {};
 
-  Glossary.url=url;
-  $.get(url,function(data) {
-    Glossary.data=data })
+    // only scan for links inside proper page scope
+    if (!container_selector) {
+        container_selector = document.body;
+    }
+
+    // get data from glossary page and tuck it into a dictionary,
+    // then validate links inside the container
+    $.get(url, function(data) {
+        Glossary.addTerms(data)
+        Glossary.validateTerms(selector, container_selector);
+    });
     
-  if (!watch_container_selector) {
-    watch_container_selector = document.body;
-  }
-    $(watch_container_selector).off("mouseover.glossary", selector);
-    $(watch_container_selector).on("mouseover.glossary", selector, function() { Glossary.popup(this) });
+    // create the popup listener
+    $(container_selector).off('mouseover.glossary', selector);
+    $(container_selector).on('mouseover.glossary', selector, function() {
+        Glossary.popup(this);
+    });
 
-  $(document.body).keydown(function(e){ // keydown instead of keypress for Chrome...
-      if(e.which == 27){
-        $('#glossarypopup').remove(); // just in case
-      }
-  });
+    // handle alternate dismissals of glossary card
+    $(document.body).keydown(function(e){ // keydown instead of keypress for Chrome...
+        if(e.which == 27){
+            $('#glossary-card').remove();
+        }
+    });
+    $(document.body).click(function(e){ 
+        $('#glossary-card').remove();
+    });
+    
+    // listen for new glossary terms on page and validate them
+    $('body').on('glossaryUpdate', function(e, selection){
+        // if the event itself gets passed a container,
+        // only scan that container for glossary terms
+        if (!!selection) {
+            Glossary.validateTerms(selector, selection);
+        } else {
+            Glossary.validateTerms(selector, container_selector);
+        }
+    });
+}
 
-  $(document.body).click(function(e){ 
-    $('#glossarypopup').remove(); // just in case
-  });
+// takes html from glossary page and builds term dictionary
+Glossary.addTerms = function(data, selector) {
+    $(data).find('dt').each(function(){
+        var term = $(this);
+        Glossary.terms[this.id.replace('term-','')] = {
+            name: term.text(),
+            definition: term.next().html()
+        };
+    });
+}
 
+// remove selector class from elements with no corresponding glossary term
+Glossary.validateTerms = function(selector, container_selector) {
+    $(container_selector).find(selector).each(function() {
+        var term = Glossary.getTermForObject(this);
+        if (!term) {
+            $(this).removeClass(selector.replace(/^[\.#]/,''));
+        }
+    });
+}
+
+// creates popup glossary card when term is hovered
+Glossary.popup = function(obj) {
+    $('#glossary-card').remove(); // just in case
+    $('body').append('<div id="glossary-card" class="hovercard"><small>&times; Close</small></div>');
+
+    var term = Glossary.getTermForObject(obj),
+        offset = $(obj).offset();
+        
+    if (!!term) {
+        $('#glossary-card')
+            .append('<dt>' + term.name + '</dt><dd>' + term.definition + '</dd>')
+            .css('top', offset.top + 25)
+            .css('left', offset.left - 25);
+    }
+}
+
+// gets keyword from HTML element, returns glossary term/definition or null
+Glossary.getTermForObject = function(obj) {
+    var keyword = $(obj).data('keyword') || obj.textContent,
+        slug = keyword.toLowerCase().replace(/[^a-z0-9-]/,'-');
+        
+    return Glossary.terms[slug]
+}
+
+// alternate init method for links that open to anchor on glossary page
+Glossary.initLinks = function (url, selector, container_selector) {
+    Glossary.url = url;
+    Glossary.terms = {};
+
+    // only scan for links inside proper page scope
+    if (!container_selector) {
+        container_selector = document.body;
+    }
+
+    // get data from glossary page and tuck it into a dictionary,
+    // then validate links inside the container
+    $.get(url, function(data) {
+        Glossary.addTerms(data)
+        Glossary.validateTerms(selector, container_selector);
+    });
+    
+    // create the popup listener
+    $(container_selector).off('click.glossary', selector);
+    $(container_selector).on('click.glossary', selector, function() {
+        Glossary.openLink(this);
+    });
 }
 
 Glossary.openLink = function(obj) {
-  var keyword = $(obj).data('keyword') || obj.textContent;
-  var slug = '#term-'+keyword.toLowerCase().replace(/[^a-z0-9-]/,'-');
-  window.open(Glossary.url + slug, "_blank");
+    var keyword = $(obj).data('keyword') || obj.textContent,
+        slug = '#term-'+keyword.toLowerCase().replace(/[^a-z0-9-]/,'-');
+
+    window.open(Glossary.url + slug, "_blank");
 }
-
-Glossary.initLinks = function (url, selector, watch_container_selector) {
-
-  Glossary.url=url;
-  $.get(url,function(data) {
-    Glossary.data=data })
-    
-  if (!watch_container_selector) {
-    watch_container_selector = document.body;
-  }
-    $(watch_container_selector).off("click.glossary", selector);
-    $(watch_container_selector).on("click.glossary", selector, function() { Glossary.openLink(this); });
-
-  }
