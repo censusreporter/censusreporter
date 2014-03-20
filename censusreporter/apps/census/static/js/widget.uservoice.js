2064f98622eb801b10edd6ab402e2101970ff8c0
Ryan Pitts <ryan.a.pitts@gmail.com>
2014-03-20 16:24:55 -0700

    remove a bunch of cruft, and jsdelivr all the things

---
+++ censusreporter/apps/census/static/js/widget.uservoice.js
@@ -0,0 +1,16 @@
+// UserVoice
+if (browserWidth > 480) {
+    UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/OWRzGZCkWqexoFNxy0iLg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+
+    UserVoice.push(['set', {
+        accent_color: '#1a9d95',
+        trigger_color: 'white',
+        trigger_background_color: '#67b3a3'
+    }]);
+
+    UserVoice.push(['addTrigger', {
+        mode: 'contact',
+        trigger_style: 'tab',
+        trigger_position: 'right'
+    }]);
+}
