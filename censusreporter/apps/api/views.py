0d1bf5ee72c9390f986cb49b98812d721fd4fabb
Rizmari Versfeld <rizziepit@gmail.com>
2014-04-01 15:25:20 +0200

    demarcation models and data - wards not related yet

--- censusreporter/apps/api/views.py
+++ censusreporter/apps/api/views.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from .config import DB_NAME, DB_USER, DB_PASSWORD
 
 _engine = create_engine("postgresql://%s:%s@localhost/%s"
                         % (DB_USER, DB_PASSWORD, DB_NAME))
+_Session = sessionmaker(bind=_engine)
 
-def get_connection():
-    return _engine.connect()
+def get_session():
+    return _Session()
