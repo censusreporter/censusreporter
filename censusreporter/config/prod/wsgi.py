51409fb84125af66094377dd8257d0c58adc6116
Rizmari Versfeld <rizziepit@gmail.com>
2014-04-01 10:09:27 +0200

    get rid of newrelic

--- censusreporter/config/prod/wsgi.py
+++ censusreporter/config/prod/wsgi.py
@@ -1,8 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-import newrelic.agent
-newrelic.agent.initialize('/home/www-data/django_app/newrelic.ini')
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.prod.settings")
 application = get_wsgi_application()
