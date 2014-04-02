4b2fb4cea0a2e5cf2a5d6ee4f8a9ccb2c8a4f298
Rizmari Versfeld <rizziepit@gmail.com>
2014-04-02 13:35:09 +0200

    cater for N-dimensional data in models and import script

--- censusreporter/apps/api/utils.py
+++ censusreporter/apps/api/utils.py
@@ -8,6 +8,9 @@ _engine = create_engine("postgresql://%s:%s@localhost/%s"
                         % (DB_USER, DB_PASSWORD, DB_NAME))
 _Session = sessionmaker(bind=_engine)
 
+# Postgres has a max name length of 63 by default
+MAX_TABLE_NAME_LENGTH = 63
+
 
 census_fields = set([
     'highest educational level',
@@ -22,7 +25,8 @@ geo_levels = (
     'ward',
     'municipality',
     'district',
-    'province'
+    'province',
+    'country',
 )
 
 
@@ -30,9 +34,24 @@ def get_session():
     return _Session()
 
 
-def get_table_name(field, geo_level):
+def get_table_name(fields, geo_level):
     if geo_level not in geo_levels:
         raise ValueError('Invalid geo_level: %s' % geo_level)
-    if field not in census_fields:
-        raise ValueError('Invalid field: %s' % field)
-    return '%s_%s' % (''.join(field.lower().split(' ')), geo_level)
+    for field in fields:
+        if field not in census_fields:
+            raise ValueError('Invalid field: %s' % field)
+
+    sorted_fields = sorted(fields)
+    table_name = ''.join(sorted_fields[0].lower().split(' '))
+    for field in sorted_fields[1:]:
+        table_name = '%s_%s' % (table_name,
+                                ''.join(field.lower().split(' ')))
+
+    table_name_length = len(table_name) + len(geo_level) + 1
+    if table_name_length > MAX_TABLE_NAME_LENGTH:
+        if table_name_length - len(sorted_fields[-1]) + 1 > MAX_TABLE_NAME_LENGTH:
+            raise RuntimeError("Table name exceeds %s characters"
+                               % MAX_TABLE_NAME_LENGTH)
+        table_name = table_name[:MAX_TABLE_NAME_LENGTH - table_name_length]
+
+    return '%s_%s' % (table_name, geo_level)
