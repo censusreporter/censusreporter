3e9a1dcc305b248b6ded5e7654a85ffdb51e5842
Rizmari Versfeld <rizziepit@gmail.com>
2014-04-04 21:26:29 +0200

    nginx and upstart configs done

--- censusreporter/config/prod/settings.py
+++ censusreporter/config/prod/settings.py
@@ -5,7 +5,4 @@ TEMPLATE_DEBUG = DEBUG
 ROOT_URLCONF = 'config.prod.urls'
 WSGI_APPLICATION = "config.prod.wsgi.application"
 
-ALLOWED_HOSTS = [
-    '174.129.183.221',
-    '.censusreporter.org',
-]
+ALLOWED_HOSTS = ['*']
--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 import os
 import pwd
-from fabric.api import cd, env, task, require, local, lcd, sudo
-from fabric.contrib.files import exists
+from fabric.api import cd, env, task, require, local, lcd, sudo, prefix, shell_env
+from fabric.contrib.files import exists, upload_template
 
 from api.fabfile import (provision_api, create_api_database, drop_api_database,
                          load_api_data)
@@ -18,6 +18,11 @@ PACKAGES = (
     'upstart',
     'nginx'
 )
+# Nginx & Upstart constants
+SERVER_NAMES = '5.9.108.98 mmm-dashboard.code4sa.org'
+PROXY_PORT = 5001
+PROXY_HOST = '127.0.0.1'
+LOG_DIR = 'censusreporter_logs'
 
 
 @task
@@ -46,16 +51,16 @@ def provision_censusreporter():
     )
     if env.deploy_type == 'prod':
         commands += (
-            'mkdir %s' % env.deploy_dir,
+            'mkdir -p %s' % env.deploy_dir,
             'chown -R %s:%s %s' % (env.deploy_user, env.deploy_user, env.deploy_dir),
         )
 
     if env.deploy_type == 'dev':
         local('; '.join('sudo %s' % cmd for cmd in commands))
-        local('sudo apt-get build-dep python-numpy --no-upgrade')
+        local('sudo apt-get build-dep python-numpy python-psycopg2 --no-upgrade')
         return
     sudo('; '.join(commands))
-    sudo('apt-get build-dep python-numpy --no-upgrade')
+    sudo('apt-get build-dep python-numpy python-psycopg2 --no-upgrade')
 
 
 @task
@@ -84,7 +89,52 @@ def deploy_censusreporter():
         with cd(repo_dir):
             sudo('git checkout -B %s' % env.branch, user=env.deploy_user)
             sudo('git pull origin %s' % env.branch, user=env.deploy_user)
-    with cd(repo_dir):
-        sudo('. ../%s/bin/activate && pip install -r requirements.txt '
-             '-r censusreporter/api/requirements.txt'
-             % VIRTUALENV_DIR, user=env.deploy_user)
+
+    # upload pth file to manage python path
+    upload_template(os.path.join(os.path.dirname(__file__), 'conf/pythonpath.template'),
+                    '%s/lib/python2.7/site-packages/censusreporter.pth' % ve_dir,
+                    {'project-dir': repo_dir}, use_sudo=True, backup=False)
+
+    with cd(repo_dir), prefix('. ../%s/bin/activate' % VIRTUALENV_DIR):
+        sudo('pip install -r requirements.txt', user=env.deploy_user)
+        sudo('pip install -r censusreporter/api/requirements.txt',
+             user=env.deploy_user)
+        sudo('python manage.py collectstatic --settings=config.prod.settings '
+             '--noinput', user=env.deploy_user)
+
+    # render and upload templates
+    upload_template(os.path.join(os.path.dirname(__file__), 'conf/nginx.template'),
+                    '/etc/nginx/sites-enabled/censusreporter.conf',
+                    get_nginx_template_context(), use_sudo=True, backup=False)
+    upload_template(os.path.join(os.path.dirname(__file__), 'conf/upstart.template'),
+                    '/etc/init/censusreporter.conf',
+                    get_upstart_template_context(), use_sudo=True, backup=False)
+    # make sure logging dir exists and update processes
+    log_dir = os.path.join(env.deploy_dir, LOG_DIR)
+    sudo('mkdir -p %s' % log_dir, user=env.deploy_user)
+    sudo('initctl restart censusreporter')
+    sudo('/etc/init.d/nginx reload')
+
+
+def get_nginx_template_context():
+    return {
+        'server-name': SERVER_NAMES,
+        'server-port': 80,
+        'static-path': os.path.join(env.deploy_dir, 'censusreporter/censusreporter/static'),
+        'log': os.path.join(env.deploy_dir, LOG_DIR, 'nginx.log'),
+        'err-log': os.path.join(env.deploy_dir, LOG_DIR, 'nginx.err'),
+        'proxy-host': PROXY_HOST,
+        'proxy-port': PROXY_PORT,
+    }
+
+
+def get_upstart_template_context():
+    return {
+        'user': env.deploy_user,
+        'project-dir': os.path.join(env.deploy_dir, CODE_DIR),
+        've-dir': os.path.join(env.deploy_dir, VIRTUALENV_DIR),
+        'log': os.path.join(env.deploy_dir, LOG_DIR, 'gunicorn.log'),
+        'err-log': os.path.join(env.deploy_dir, LOG_DIR, 'gunicorn.err'),
+        'host': PROXY_HOST,
+        'port': PROXY_PORT,
+    }
