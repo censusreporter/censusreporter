56ea696d918539a377cb0d7fc351d93e993a3acb
Rizmari Versfeld <rizziepit@gmail.com>
2014-04-10 16:48:25 +0200

    add votes model and data (compressed)

--- censusreporter/api/controller.py
+++ censusreporter/api/controller.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql.expression import or_, and_
 from sqlalchemy import func
 
 from .models import (get_model_from_fields, Ward, District, Municipality,
-                     Province)
+                     Province, Votes)
 from .utils import get_session, ward_search_api, geo_levels
 
 
@@ -190,7 +190,7 @@ def get_geography(geo_code, geo_level):
         session.close()
 
 
-def get_profile(geo_code, geo_level):
+def get_census_profile(geo_code, geo_level):
     session = get_session()
     data = {}
     for section in PROFILE_SECTIONS:
@@ -720,3 +720,23 @@ def _complete_ward_data_from_api(locations, session):
             ward_obj.district_code = municipality.district_code
 
     session.commit()
+
+
+def get_elections_profile(geo_code, geo_level):
+    session = get_session()
+
+    try:
+        field_name = {
+            'ward': 'ward_code',
+            'province': 'province_code',
+            'municipality': 'municipality_code',
+            'district': 'district_code'
+        }[geo_level]
+        query = session.query(Votes).filter(getattr(Votes, field_name)
+                                            == geo_code)
+        
+        
+    except KeyError:
+        raise ValueError('Invalid geo_level: %s' % geo_level)
+    finally:
+        session.close()
