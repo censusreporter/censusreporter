f51614a0a645a04968efea2fc34c70ebf620c212
Rizmari Versfeld <rizziepit@gmail.com>
2014-04-11 17:03:09 +0200

    basic municipal elections in profile

--- censusreporter/api/models.py
+++ censusreporter/api/models.py
@@ -136,14 +136,23 @@ class Votes(Base):
     # an 8-digit code
     # Note: a voting district is at sub-ward level
     voting_district_code = Column(String(8), primary_key=True)
+    # current only 'municipal 2011'
     electoral_event = Column(String(32), primary_key=True)
     party = Column(String(64), primary_key=True)
+    # one of PR (provincial), WARD or DC 40%
     ballot_type = Column(String(8), primary_key=True)
+    # register votes in whole voting district
     registered_voters = Column(Integer)
+    # total votes in whole voting district per ballot type
     total_votes = Column(Integer)
+    # valid votes for the particular party per ballot type
+    # we are mostly interested in this
     valid_votes = Column(Integer)
+    # spoilt votes in whole voting district per ballot type
     spoilt_votes = Column(Integer)
+    # mec7 votes in whole voting district - only valid for provincial ballots
     mec7_votes = Column(Integer)
+    # percentage in whole voting district
     # This value is a percentage close to, but not equal to, (total_votes / registered_voters * 100)
     # The Electoral Commission must have some other way of calculating it.
     voter_turnout = Column(Numeric(precision=5, scale=2))
@@ -153,6 +162,7 @@ class Votes(Base):
     municipality  = relationship('Municipality', lazy=True)
     province      = relationship('Province', lazy=True)
     district  = relationship('District', lazy=True)
+
 
 '''
 Census data models
