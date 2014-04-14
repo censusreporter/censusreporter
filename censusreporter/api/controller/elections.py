23886447b90800146c72dbbec587bc8807199eee
Rizmari Versfeld <rizziepit@gmail.com>
2014-04-14 20:55:35 +0200

    allow all ballot types and country-level election stats

--- censusreporter/api/controller/elections.py
+++ censusreporter/api/controller/elections.py
@@ -6,6 +6,13 @@ from api.models import Votes
 from api.utils import get_session
 
 
+BALLOT_TYPE_DESCRIPTION = {
+    'PR': 'Proportional representative (PR)',
+    'WARD': 'Ward',
+    'DC 40%': 'District council (DC) 40%'
+}
+
+
 def make_party_acronym(name):
     '''
     This is good enough since only 2 parties have the same acronym,
@@ -27,7 +34,8 @@ def make_party_acronym(name):
         return acronym
 
 
-def get_elections_profile(geo_code, geo_level, election="municipal 2011"):
+def get_elections_profile(geo_code, geo_level, election="municipal 2011",
+                          ballot_type='PR'):
     session = get_session()
 
     try:
@@ -35,27 +43,34 @@ def get_elections_profile(geo_code, geo_level, election="municipal 2011"):
             'ward': 'ward_code',
             'province': 'province_code',
             'municipality': 'municipality_code',
-            'district': 'district_code'
+            'district': 'district_code',
+            'country': None,
         }[geo_level]
 
         # only consider proportional representative (PR) votes for now
         votes_by_party = session \
                 .query(Votes.party, func.sum(Votes.valid_votes).label('vote_sum')) \
-                .filter(getattr(Votes, field_name) == geo_code) \
-                .filter(Votes.ballot_type == 'PR') \
+                .filter(Votes.ballot_type == ballot_type) \
                 .filter(Votes.electoral_event == election) \
                 .group_by(Votes.party) \
                 .having(func.sum(Votes.valid_votes) > 0) \
-                .order_by(desc('vote_sum')) \
-                .all()
+                .order_by(desc('vote_sum'))
+
+        if geo_level != 'country':
+            votes_by_party = votes_by_party.filter(getattr(Votes, field_name)
+                                                   == geo_code)
+
         subquery = session \
                 .query(Votes.total_votes, Votes.registered_voters,
                        Votes.voter_turnout, Votes.spoilt_votes) \
-                .filter(getattr(Votes, field_name) == geo_code) \
-                .filter(Votes.ballot_type == 'PR') \
+                .filter(Votes.ballot_type == ballot_type) \
                 .filter(Votes.electoral_event == election) \
                 .distinct(Votes.voting_district_code) \
-                .subquery() 
+
+        if geo_level != 'country':
+            subquery = subquery.filter(getattr(Votes, field_name) == geo_code)
+
+        subquery = subquery.subquery()
         total_votes, spoilt_votes, registered_voters, average_turnout = session \
                 .query(func.sum(subquery.c.total_votes),
                        func.sum(subquery.c.spoilt_votes),
@@ -94,7 +109,7 @@ def get_elections_profile(geo_code, geo_level, election="municipal 2011"):
             )}
 
         party_data['metadata'] = {
-            'universe': 'Proportional representative (PR) ballots only'
+            'universe': '%s ballots only' % BALLOT_TYPE_DESCRIPTION[ballot_type]
         }
 
         return {
