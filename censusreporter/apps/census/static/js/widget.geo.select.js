61075546ecbeeface222364ed5b50387bcf3693b
Rizmari Versfeld <rizmari@praekelt.com>
2014-04-17 13:59:06 +0200

    basic support for 2 typeahead sources

--- censusreporter/apps/census/static/js/widget.geo.select.js
+++ censusreporter/apps/census/static/js/widget.geo.select.js
@@ -1,33 +1,53 @@
-var geoSearchAPI = '/place-search/json/',
+var textmatchAPI = '/place-search/json/',
+    geocodingAPI = 'http://wards.code4sa.org/',
+    resultTemplate = '<p class="result-name"><span class="result-type">{{geo_level}}</span>{{full_name}}</p>',
     geoSelect = $('#geography-select');
 
 function makeGeoSelectWidget(element) {
-    element.typeahead({
-        name: 'profile',
+    baseAttributes = {
         valueKey: 'full_geoid',
         nameKey: 'full_name',
+        limit: 20,
+        template: resultTemplate,
+        engine: Hogan
+    }
+
+    // get textual matches from host
+    element.typeahead($.extend({}, baseAttributes, {
+        name: 'textmatch',
         remote: {
-            url: geoSearchAPI,
-            replace: function (url, uriEncodedQuery) {
+            url: textmatchAPI,
+            replace: function(url, uriEncodedQuery) {
                 return url += '?q=' + uriEncodedQuery;
             },
             filter: function(response) {
-                var results = response.results;
-                results.map(function(item) {
-                    item['geo_level'] = item['full_geoid'].split('-')[0];
-                });
-                return results;
+                return response.results;
             }
-        },
-        limit: 20,
-        template: '<p class="result-name"><span class="result-type">{{geo_level}}</span>{{full_name}}</p>',
-        engine: Hogan
-    });
+        }
+    }));
+
+    // get geocoded matches from Code4SA API
+    element.typeahead($.extend({}, baseAttributes, {
+        name: 'geocoded',
+        remote: {
+            url: geocodingAPI,
+            replace: function(url, uriEncodedQuery) {
+                return url += '?address=' + uriEncodedQuery + '&database=wards_2011';
+            },
+            filter: function(response) {
+                response.map(function(item) {
+                });
+                return [];
+            },
+            dataType: 'jsonp',  // allow cross domain request
+        }
+    }));
 
     element.on('typeahead:selected', function(obj, datum) {
         element.typeahead('setQuery', datum['full_name']);
         window.location = '/profiles/' + datum['full_geoid'] + '/';
     });
+
 }
 
 makeGeoSelectWidget(geoSelect);
