d2abb6b438e16ace47c47b650dc8c6322a66d485
Greg Kempe <greg@kempe.net>
2014-04-17 12:20:33 +0200

    Search looks at local data only.
    This is missing anything better than municipality names.
    
    Free-form geocoded search will use the ward.code4sa.org API

--- censusreporter/api/controller/geography.py
+++ censusreporter/api/controller/geography.py
@@ -39,108 +39,41 @@ def get_locations(search_term, geo_level=None, year='2011'):
         raise ValueError('Invalid geo_level: %s' % geo_level)
     session = get_session()
 
-    if geo_level == 'ward':
-        # try to find by ward code first, then address/place name
-        ward = session.query(Ward).get(search_term)
-        if not ward:
-            locations = ward_search_api.search(search_term)
-            if locations:
-                ward_codes = [l.ward_code for l in locations]
-                wards = session \
-                        .query(Ward) \
-                        .filter(Ward.code.in_(ward_codes)) \
-                        .filter(Ward.year == year) \
-                        .all()
-            else:
-                wards = []
-        else:
-            wards = [ward]
-        return serialize_demarcations(wards)
+    if geo_level:
+        levels = [geo_level]
+    else:
+        levels = ['province', 'municipality']
 
-    elif geo_level is not None:
+    objects = set()
+
+    # search at each level
+    for level in levels:
         # already checked that geo_level is valid
         model = {
-            'district': District,
             'municipality': Municipality,
             'province': Province,
-        }[geo_level]
-        # try to find by code or name
-        demarcations = session \
-                .query(model) \
-                .filter(model.year == year) \
-                .filter(or_(model.name.ilike(search_term + '%'),
-                            model.code == search_term.upper())) \
-                .all()
-        return serialize_demarcations(demarcations)
+        }[level]
 
-    else:
-        '''
-        This search differs from the above in that it first
-        search for wards. If it finds wards it adds the wards and
-        their provinces, districts and municipalities to the results.
-        It then also checks if any province, district or municipality
-        matches the search term in their own right, adding these
-        to the results as well.
-        '''
-        objects = set()
-        # look up wards
-        locations = ward_search_api.search(search_term)
-        if locations:
-            ward_codes = [l.ward_code for l in locations]
-            wards = session \
-                    .query(Ward) \
-                    .options(joinedload('*', innerjoin=True)) \
-                    .filter(Ward.code.in_(ward_codes)) \
-                    .filter(Ward.year == year) \
-                    .all()
-            objects.update(wards)
-            for ward in wards:
-                objects.update([ward.municipality, ward.district, ward.province])
+        objects.update(session
+            .query(model)
+            .filter(model.year == year)
+            .filter(or_(model.name.ilike(search_term + '%'),
+                        model.name.ilike('City of %s' % search_term + '%'),
+                        model.code == search_term.upper()))
+            .limit(10)
+        )
 
-        # find other matches
-        for model in (Municipality, District, Province):
-            objects.update(session
-                .query(model)
-                .filter(model.year == year)
-                .filter(or_(model.name.ilike(search_term + '%'),
-                            model.name.ilike('City of %s' % search_term + '%'),
-                            model.code == search_term.upper()))
-                .all()
-            )
 
-        order_map = {Ward: 1, Municipality: 2, District: 3, Province: 4}
-        objects = sorted(objects, key=lambda o: "%d%s" % (
-            order_map[o.__class__],
-            getattr(o, 'name', getattr(o, 'code'))
-        ))
-        return serialize_demarcations(objects)
+    order_map = {Ward: 3, Municipality: 2, Province: 1}
+    objects = sorted(objects, key=lambda o: [order_map[o.__class__], getattr(o, 'name', getattr(o, 'code'))])
+
+    return serialize_demarcations(objects[0:10])
 
 
 def serialize_demarcations(objects):
-    data = []
-    for obj in objects:
-        if isinstance(obj, Ward):
-            obj_dict = {
-                'full_name': '%s, %s, %s' % (obj.code, obj.municipality.name,
-                                             obj.province_code),
-                'full_geoid': 'ward-%s' % obj.code,
-            }
-        elif isinstance(obj, Municipality):
-            obj_dict = {
-                'full_name': '%s, %s' % (obj.name, obj.province_code),
-                'full_geoid': 'municipality-%s' % obj.code,
-            }
-        elif isinstance(obj, District):
-            obj_dict = {
-                'full_name': '%s, %s' % (obj.name, obj.province_code),
-                'full_geoid': 'district-%s' % obj.code,
-            }
-        elif isinstance(obj, Province):
-            obj_dict = {
-                'full_name': '%s' % obj.name,
-                'full_geoid': 'province-%s' % obj.code,
-            }
-        else:
-            raise ValueError("Unrecognized demarcation class")
-        data.append(obj_dict)
-    return data
+    return [{
+            'full_name': obj.long_name,
+            'full_geoid': '%s-%s' % (obj.level, obj.code),
+            'geo_level': obj.level,
+            'geo_code': obj.code,
+            } for obj in objects]
