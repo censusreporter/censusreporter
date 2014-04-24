c1800e2cc9285321fd57333f8e04530c5936064e
Rizmari Versfeld <rizmari@praekelt.com>
2014-04-24 09:12:01 +0200

    change 'Citizens' to 'People'

--- censusreporter/api/controller/census.py
+++ censusreporter/api/controller/census.py
@@ -255,7 +255,7 @@ def get_demographics_profile(geo_code, geo_level, session):
             }),
         )),
         'total_population': {
-            "name": "Citizens",
+            "name": "People",
             "values": {"this": total_pop}
         }}
 
