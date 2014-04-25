cd833991ea8b3830415dccad57f92ac544cbf3ad
Ian Dees <ian.dees@gmail.com>
2014-04-24 22:41:46 -0500

    Push to S3.

--- censusreporter/apps/census/management/commands/cache_to_s3.py
+++ censusreporter/apps/census/management/commands/cache_to_s3.py
@@ -1,20 +1,54 @@
 from django.core.management.base import BaseCommand
 from multiprocessing import Pool
 from traceback import format_exc
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
 import json
+import cStringIO
+import gzip
 
 from ...profile import geo_profile, enhance_api_data
+
+s3 = S3Connection()
+
+def s3_keyname(geoid):
+    return '/1.0/data/profiles/%s.json' % geoid
+
+def key(geoid):
+    bucket = s3.get_bucket('embed.censusreporter.org')
+    keyname = s3_keyname(geoid)
+    key = Key(bucket, keyname)
+
+    return key
+
+def write_profile_json(s3_key, data):
+    s3_key.metadata['Content-Type'] = 'application/json'
+    s3_key.metadata['Content-Encoding'] = 'gzip'
+    s3_key.storage_class = 'REDUCED_REDUNDANCY'
+
+    # create gzipped version of json in memory
+    memfile = cStringIO.StringIO()
+    #memfile.write(data)
+    with gzip.GzipFile(filename=s3_key.key, mode='wb', fileobj=memfile) as gzip_data:
+        gzip_data.write(data)
+    memfile.seek(0)
+
+    # store static version on S3
+    s3_key.set_contents_from_file(memfile)
 
 def seed(geoid):
     print "Working on {}".format(geoid)
     try:
         api_data = geo_profile(geoid)
         api_data = enhance_api_data(api_data)
+
+        s3key = key(geoid)
+        write_profile_json(s3key, json.dumps(api_data))
     except Exception, e:
         format_exc(e)
     print "Done working on {}".format(geoid)
 
-    print json.dumps(api_data)
 
 class Command(BaseCommand):
     help = 'Pre-generates some Census Reporter content and places it on S3.'
