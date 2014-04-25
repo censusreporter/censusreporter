d10b7aba9409101f9c0c94ce1d6bf60a09852d18
Ian Dees <ian.dees@gmail.com>
2014-04-24 23:01:46 -0500

    Bump the log level back.

--- censusreporter/apps/census/management/commands/cache_to_s3.py
+++ censusreporter/apps/census/management/commands/cache_to_s3.py
@@ -11,7 +11,7 @@ import gzip
 from ...profile import geo_profile, enhance_api_data
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 s3 = S3Connection()
