97ec1052e68fe9de4e176aade148f184dffbb9b4
Greg Kempe <greg@kempe.net>
2014-05-07 14:37:56 +0200

    Use upstream's method of invoking newrelic.

--- censusreporter/config/prod/wsgi.py
+++ censusreporter/config/prod/wsgi.py
@@ -1,5 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+import newrelic.agent
+newrelic.agent.initialize('/var/www-data/censusreporter/conf/newrelic.ini')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.prod.settings")
 application = get_wsgi_application()
