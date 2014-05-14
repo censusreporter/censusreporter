64968b61c4b113769dc14fcc281793829c581d9f
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-05-14 11:24:31 +0200

    Hotfix: give postMessage the correct url (http://embed.wazimap.co.za).

--- censusreporter/apps/census/static/js/embed.chart.make.js
+++ censusreporter/apps/census/static/js/embed.chart.make.js
@@ -68,7 +68,7 @@ function makeCensusEmbeds() {
     embed.sendDataToFrames = function(data) {
         // IE9 can only send strings
         for (var i = 0; i < embed.numContainers; i++) {
-            embed.containers[i].contentWindow.postMessage(JSON.stringify(data), 'http://wazimap.co.za');
+            embed.containers[i].contentWindow.postMessage(JSON.stringify(data), 'http://embed.wazimap.co.za');
         }
     }
     
