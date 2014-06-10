c7bfb6578dc98a76c98bd6ba8b4912472b66f92f
Greg Kempe <greg@kempe.net>
2014-06-10 09:43:15 +0200

    Re-add uservoice

--- censusreporter/apps/census/static/js/widget.uservoice.js
+++ censusreporter/apps/census/static/js/widget.uservoice.js
@@ -1,6 +1,6 @@
 // UserVoice
 if (browserWidth > 480) {
-    UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/OWRzGZCkWqexoFNxy0iLg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
+    UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/geixSwRIfj9BPIlJiPWJwQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
 
     UserVoice.push(['set', {
         accent_color: '#1a9d95',
