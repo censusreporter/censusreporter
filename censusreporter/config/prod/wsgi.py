d67099ce7d30e31b98251f7386b33caaa5199a01
Greg Kempe <greg@kempe.net>
2014-07-28 12:16:02 +0200

    Correct location of newrelic config

--- censusreporter/config/prod/wsgi.py
+++ censusreporter/config/prod/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 import newrelic.agent
-newrelic.agent.initialize('/var/www-data/censusreporter/conf/newrelic.ini')
+newrelic.agent.initialize(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../../conf/newrelic.ini'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.prod.settings")
 application = get_wsgi_application()
