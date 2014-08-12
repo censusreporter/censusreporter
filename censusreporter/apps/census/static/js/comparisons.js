b4915069d0cd6d92837808f2320bda7b6e9232a9
Greg Kempe <greg@kempe.net>
2014-08-12 12:35:35 +0200

    Use mapnik B&W map

--- censusreporter/apps/census/static/js/comparisons.js
+++ censusreporter/apps/census/static/js/comparisons.js
@@ -162,8 +162,8 @@ function Comparison(options) {
             }
 
             // add imagery
-            L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-              attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+            L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
+              attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
               subdomains: 'abcd',
               maxZoom: 17
             }).addTo(comparison.map);
