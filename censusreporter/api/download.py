e3e39f04608ea779b8383e8e21079c863f9d703f
Greg Kempe <greg@kempe.net>
2014-08-15 08:38:12 +0200

    Pull in generate_download_bundle from KE, it's better

--- censusreporter/api/download.py
+++ censusreporter/api/download.py
@@ -1,29 +1,27 @@
 import shutil
 import tempfile
 import os
+from osgeo import ogr, osr
 
 supported_formats = {
-    #'shp':      {"driver": "ESRI Shapefile"},
-    #'kml':      {"driver": "KML"},
-    #'geojson':  {"driver": "GeoJSON"},
-    'xlsx':     {"driver": "XLSX", 'mime': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'},
-    'csv':      {"driver": "CSV", 'mime': 'text/csv'},
+    'kml':      {"driver": "KML",     'geometry': True, 'mime': 'application/vnd.google-earth.kml+xml'},
+    'geojson':  {"driver": "GeoJSON", 'geometry': True, 'mime': 'application/json'},
+    'xlsx':     {"driver": "XLSX",    'geometry': False, 'mime': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'},
+    'csv':      {"driver": "CSV",     'geometry': False, 'mime': 'text/csv'},
 }
 
 def generate_download_bundle(tables, geos, data, fmt):
-    import ogr
-    import osr
-
     # where we're going to put the data temporarily
     temp_path = tempfile.mkdtemp()
     try:
         file_ident = "%s_%s" % (tables[0].id.upper(), geos[0].short_name)
         out_filename = '%s.%s' % (file_ident, fmt)
         out_filepath = os.path.join(temp_path, out_filename)
+        format = supported_formats[fmt]
 
         ogr.UseExceptions()
 
-        out_driver = ogr.GetDriverByName(supported_formats[fmt]['driver'])
+        out_driver = ogr.GetDriverByName(format['driver'])
         out_srs = osr.SpatialReference()
         out_srs.ImportFromEPSG(4326)
         out_data = out_driver.CreateDataSource(out_filepath)
@@ -40,34 +38,62 @@ def generate_download_bundle(tables, geos, data, fmt):
                 for column_id, column_info in table.columns.iteritems():
                     out_layer.CreateField(ogr.FieldDefn(str(column_id), ogr.OFTReal))
 
-            for geo in geos:
-                geoid = geo.full_geoid
+            geometries = load_geometries(set(g.level for g in geos))
+            try:
+                for geo in geos:
+                    geoid = geo.full_geoid
 
-                out_feat = ogr.Feature(out_layer.GetLayerDefn())
-                # XXX: need to get the geometry from somewhere
-                #out_feat.SetGeometry(in_feat.GetGeometryRef())
-                out_feat.SetField2('geo_level', geo.level)
-                out_feat.SetField2('geo_code', geo.code)
-                out_feat.SetField2('geoid', geoid)
-                out_feat.SetField2('name', geo.short_name)
+                    out_feat = ogr.Feature(out_layer.GetLayerDefn())
 
-                for table in tables:
-                    table_estimates = data[geoid][table.id.upper()]['estimate']
+                    if format['geometry']:
+                        geom = get_geometry(geo, geometries)
+                        out_feat.SetGeometry(geom.GetGeometryRef())
 
-                    for column_id, column_info in table.columns.iteritems():
-                        if column_id in table_estimates:
-                            # GDAL generates invalid excel spreadsheets for
-                            # zero values in real columns
-                            if fmt == 'xlsx' and table_estimates[column_id] == 0:
-                                continue
-                            out_feat.SetField(str(column_id), table_estimates[column_id])
+                    out_feat.SetField2('geo_level', geo.level)
+                    out_feat.SetField2('geo_code', geo.code)
+                    out_feat.SetField2('geoid', geoid)
+                    out_feat.SetField2('name', geo.short_name)
 
-                out_layer.CreateFeature(out_feat)
+                    for table in tables:
+                        table_estimates = data[geoid][table.id.upper()]['estimate']
+
+                        for column_id, column_info in table.columns.iteritems():
+                            if column_id in table_estimates:
+                                # GDAL generates invalid excel spreadsheets for
+                                # zero values in real columns
+                                if fmt == 'xlsx' and table_estimates[column_id] == 0:
+                                    continue
+                                out_feat.SetField(str(column_id), table_estimates[column_id])
+
+                    out_layer.CreateFeature(out_feat)
+            finally:
+                for source in geometries.itervalues():
+                    source.Destroy()
         finally:
             out_data.Destroy()
 
         with open(out_filepath) as f:
             content = f.read()
-            return content, out_filename, supported_formats[fmt]['mime']
+            return content, out_filename, format['mime']
     finally:
         shutil.rmtree(temp_path)
+
+def get_geometry(geo, geometries):
+    source = geometries[geo.level]
+    layer = source.GetLayer(0)
+    for i in xrange(layer.GetFeatureCount()):
+        feat = layer.GetFeature(i)
+        if feat['geoid'] == geo.full_geoid:
+            return feat
+
+def load_geometries(levels):
+    geom = {}
+    driver = ogr.GetDriverByName('GeoJSON')
+
+    path = os.path.join(os.path.dirname(__file__), '../apps/census/static/geo/')
+    for level in levels:
+        fname = os.path.join(path, level + '.geojson')
+        ds = driver.Open(fname)
+        geom[level] = ds
+
+    return geom
