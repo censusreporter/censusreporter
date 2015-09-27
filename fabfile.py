754033c22c8e37b02998fe9ddf4c5e6d5086f9ae
Ian Dees <ian.dees@gmail.com>
2015-09-27 10:41:12 -0400

    Update the fabfile and surrounds to support nginx/gunicorn

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,9 @@ from fabric.api import *
 from fabric.contrib.files import *
 
 root_dir = '/home/www-data'
-code_dir = '%s/django_app' % root_dir
-virtualenv_name = 'django_venv'
+host = 'censusreporter.org'
+code_dir = '%s/%s_app' % (root_dir, host)
+virtualenv_name = '%s_venv' % host
 virtualenv_dir = '%s/%s' % (root_dir, virtualenv_name)
 newrelic_app_name = 'Census Reporter Django'
 
@@ -15,58 +16,65 @@ def install_newrelic(api_key):
 
     sudo("sed -i \"s/Python Application/%s/g\" %s/newrelic.ini" % (newrelic_app_name, code_dir), user='www-data')
 
-def deploy(branch='master'):
-    "Deploy the specified branch to the remote host."
-
-    host = 'censusreporter.org'
+def initial_config():
+    """ Configure the remote host to run Census Reporter. """
 
     sudo('mkdir -p %s' % root_dir)
     sudo('chown www-data:www-data %s' % root_dir)
 
-    # Install required packages
-    sudo('apt-get update')
-    sudo('apt-get install -y git')
-
-    # Install and set up apache and mod_wsgi
-    sudo('apt-get install -y apache2 libapache2-mod-wsgi')
-    sudo('a2enmod wsgi')
-    sudo('rm -f /etc/apache2/sites-enabled/000-default')
-    sudo('rm -f /etc/apache2/sites-enabled/%s' % host)
-    sudo('rm -f /etc/apache2/sites-available/%s' % host)
-    upload_template('./server/apache2/site', '/etc/apache2/sites-available/%s' % host, use_sudo=True, context={
-        'domainname': host,
-        'django_project_path': '%s/censusreporter' % code_dir,
-        'django_static_path': '%s/censusreporter/apps/census/static' % code_dir,
-        'django_venv_path': '%s/lib/python2.7/site-packages' % virtualenv_dir
-    })
-    sudo('a2ensite %s' % host)
-
     # Install up to virtualenv
-    sudo('apt-get install -y python-setuptools')
+    sudo('apt-get install -q -y python-setuptools')
     sudo('easy_install pip')
-    sudo('pip install virtualenv')
+    sudo('pip --quiet install virtualenv')
 
-    # Create virtualenv and add our django app to its PYTHONPATH
-    sudo('virtualenv --no-site-packages %s' % virtualenv_dir)
-    sudo('rm -f %s/lib/python2.7/site-packages/censusreporter.pth' % virtualenv_dir)
+    # Create virtualenv and add our Flask app to it
+    sudo('virtualenv --no-site-packages %s' % virtualenv_dir, user='www-data')
+    sudo('rm -f %s/lib/python2.7/site-packages/censusreporter.pth' % virtualenv_dir, user='www-data')
     append('%s/lib/python2.7/site-packages/censusreporter.pth' % virtualenv_dir, '%s/censusreporter' % code_dir, use_sudo=True)
     append('%s/lib/python2.7/site-packages/censusreporter.pth' % virtualenv_dir, '%s/censusreporter/apps' % code_dir, use_sudo=True)
-    append('%s/bin/activate' % virtualenv_dir, 'export DJANGO_SETTINGS_MODULE="config.prod.settings"', use_sudo=True)
+    sudo('chown www-data:www-data %s/lib/python2.7/site-packages/censusreporter.pth' % virtualenv_dir)
+
+    # Install and set up gunicorn in the virtualenv
+    with prefix('source %s/bin/activate' % virtualenv_dir):
+        sudo('pip --quiet install gunicorn futures', user='www-data')
+
+    sudo('rm -f /etc/init/%s.conf' % host)
+    upload_template('./server/upstart.conf.template', '/etc/init/%s.conf' % host, use_sudo=True, context={
+        'domainname': host,
+        'project_path': code_dir,
+        'virtualenv_dir': virtualenv_dir,
+    })
+
+    # Configure nginx to proxy requests to gunicorn
+    sudo('rm -f /etc/nginx/sites-enabled/default')
+    sudo('rm -f /etc/nginx/sites-enabled/%s' % host)
+    sudo('rm -f /etc/nginx/sites-available/%s' % host)
+    upload_template('./server/nginx.site.template', '/etc/nginx/sites-available/%s' % host, use_sudo=True, context={
+        'domainname': host,
+        'project_path': code_dir,
+    })
+    sudo('ln -s /etc/nginx/sites-available/%s /etc/nginx/sites-enabled' % host)
 
     with settings(warn_only=True):
-        if sudo('test -d %s' % code_dir).failed:
-            sudo('git clone git://github.com/censusreporter/censusreporter.git %s' % code_dir)
+        if sudo('test -d %s' % code_dir, user='www-data').failed:
+            sudo('git clone git://github.com/censusreporter/censusreporter.git %s' % code_dir, user='www-data')
+
+    # Start gunicorn
+    sudo('start %s' % host)
+
+    # Restart nginx
+    sudo('service nginx restart')
+
+def deploy(branch='master'):
+    "Deploy the specified branch to the remote host."
 
     with cd(code_dir):
-        sudo('git pull origin %s' % branch)
+        sudo('find . -name \'*.pyc\' -delete', user='www-data')
+        sudo('git pull origin %s' % branch, user='www-data')
 
         # Install pip requirements
-        sudo('source %s/bin/activate && pip install -r requirements.txt' % virtualenv_dir)
+        with prefix('source %s/bin/activate' % virtualenv_dir):
+            sudo('pip --quiet --no-cache-dir install -r requirements.txt', user='www-data')
 
-        # Make sure everything is correctly owned
-        sudo('chown www-data:www-data -R %s %s' % (code_dir, virtualenv_dir))
-
-    # Clear the cache
-    sudo('echo \'flush_all\' | nc localhost 11211')
-    # Restart apache
-    sudo('service apache2 restart')
+    # Restart gunicorn
+    sudo('restart %s' % host)
