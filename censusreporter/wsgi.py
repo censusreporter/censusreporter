275910600ea6c4d79fa5a1b44e0dd7fea1269780
Ian Dees <ian.dees@gmail.com>
2015-09-27 11:27:34 -0400

    There already was a wsgi file!

--- censusreporter/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.prod.settings")
-
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
