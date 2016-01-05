6c0212b004aef96c305406352810bd40f3d5500e
Ian Dees <ian.dees@gmail.com>
2016-01-05 11:18:06 -0600

    Add support for ELB to ALLOWED_HOSTS

--- censusreporter/config/prod/settings.py
+++ censusreporter/config/prod/settings.py
@@ -5,11 +5,22 @@ TEMPLATE_DEBUG = DEBUG
 ROOT_URLCONF = 'config.prod.urls'
 WSGI_APPLICATION = "config.prod.wsgi.application"
 
+
 ALLOWED_HOSTS = [
-    '174.129.183.221',
-    '54.173.179.176',
     '.censusreporter.org',
+    '.compute-1.amazonaws.com',  # allows viewing of instances directly
 ]
+
+# From https://dryan.com/articles/elb-django-allowed-hosts/
+import requests
+EC2_PRIVATE_IP = None
+try:
+    EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
+except requests.exceptions.RequestException:
+    pass
+
+if EC2_PRIVATE_IP:
+    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
 
 CACHES = {
     'default': {
