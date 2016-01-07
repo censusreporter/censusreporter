9e0633207c5fc881821e9be1b8db34bc609d582d
Ian Dees <ian.dees@gmail.com>
2016-01-07 12:19:19 -0500

    Add back the EC2 metadata-based private IP fetch for ALLOWED_HOSTS

--- censusreporter/config/prod/settings.py
+++ censusreporter/config/prod/settings.py
@@ -12,6 +12,19 @@ ALLOWED_HOSTS = [
     'cr-prod-409865157.us-east-1.elb.amazonaws.com',  # from the load balancer
 ]
 
+# From https://forums.aws.amazon.com/thread.jspa?messageID=423533:
+# "The Elastic Load Balancer HTTP health check will use the instance's internal IP."
+# From https://dryan.com/articles/elb-django-allowed-hosts/
+import requests
+EC2_PRIVATE_IP = None
+try:
+    EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
+except requests.exceptions.RequestException:
+    pass
+
+if EC2_PRIVATE_IP:
+    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
