64ffa95fdeb7f0063e847423b9ef7889a61b4aed
Ian Dees <ian.dees@gmail.com>
2016-01-07 11:58:07 -0500

    Just hardcode the ELB hostname instead of asking for local-ipv4 from EC2 metadata

--- censusreporter/config/prod/settings.py
+++ censusreporter/config/prod/settings.py
@@ -9,18 +9,8 @@ WSGI_APPLICATION = "config.prod.wsgi.application"
 ALLOWED_HOSTS = [
     '.censusreporter.org',
     '.compute-1.amazonaws.com',  # allows viewing of instances directly
+    'cr-prod-409865157.us-east-1.elb.amazonaws.com',  # from the load balancer
 ]
-
-# From https://dryan.com/articles/elb-django-allowed-hosts/
-import requests
-EC2_PRIVATE_IP = None
-try:
-    EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
-except requests.exceptions.RequestException:
-    pass
-
-if EC2_PRIVATE_IP:
-    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
 
 CACHES = {
     'default': {
