974e64e7d00b5203b563da1a6181988580b9e90c
Ian Dees <ian.dees@gmail.com>
2016-02-12 17:54:27 -0500

    Misplaced paren

--- censusreporter/config/base/settings.py
+++ censusreporter/config/base/settings.py
@@ -115,5 +115,5 @@ import raven
 
 RAVEN_CONFIG = {
     'dsn': os.environ.get("SENTRY_DSN"),
-    'release': raven.fetch_git_sha(os.path.dirname(__file__ + '/../../..')),
+    'release': raven.fetch_git_sha(os.path.dirname(__file__) + '/../../..'),
 }
