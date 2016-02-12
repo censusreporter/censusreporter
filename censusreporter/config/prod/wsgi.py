f8d20b650463ecc389e9ae6e0e49d76710471a49
Ian Dees <ian.dees@gmail.com>
2016-02-12 17:53:47 -0500

    Wrap the Django WSGI with Sentry's wrapper

--- censusreporter/config/prod/wsgi.py
+++ censusreporter/config/prod/wsgi.py
@@ -1,8 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 import newrelic.agent
 newrelic.agent.initialize('newrelic.ini')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.prod.settings")
-application = get_wsgi_application()
+application = Sentry(get_wsgi_application())
