0155bbd9cd4fda4a6c74d63e56d07cc1a39d5a5a
Joon Park <JoonPark2017@u.northwestern.edu>
2016-07-18 14:03:59 -0500

    Change full-text search page to front page duplicate.
    Also add the "Don't see what you're looking for?" skeleton at the bottom
    of the autosuggest results. Actual functionality not yet implemented.
    The autosuggest focus still needs to be styled correctly.

---
+++ censusreporter/apps/census/static/js/full-text.search.js
@@ -0,0 +1,69 @@
+$(function() {
+    // Emulate real front page
+    $("#explore-topic-place-picker").css("display", "none");
+    $("#explore-topic-place-picker-parent").css("display", "none");
+
+
+    // Initialize autocomplete
+    $("#geography-select-full-text").autocomplete({
+        // Grab source from ajax call
+        source: function (request, response) {
+            $.ajax({
+                // request.term is the current text in the search box.
+                url: "http://0.0.0.0:5000/2.1/full-text/search?q=" + request.term,
+                dataType: "json",
+                // Use success handler to process data before passing it to response
+                // function.
+                success: function (data) {
+                    // Format of data is {results: [ list of objects ]}
+                    var result = [];
+                    // For each returned page, prepare data for display. label and
+                    // value are defaults preferred by jQuery UI autocomplete.
+                    for (var i = 0; i < data.results.length; i++) {
+                        if (data.results[i].type == "profile") {
+                            result.push({
+                                label: data.results[i].full_name,
+                                value: data.results[i].full_name,
+                                subline: "<i>" + data.results[i].sumlevel_name + "<i>",
+                                url: data.results[i].url
+                            });
+                        } else if (data.results[i].type == "table") {
+                            result.push({
+                                label: data.results[i].table_name,
+                                value: data.results[i].table_name,
+                                subline: "<b>Table topics: </b>" + data.results[i].topics,
+                                url: data.results[i].url
+                            });
+                        }
+                    }
+
+                    // Add to end of search results
+                    result.push({
+                        label: "Don't see what you're looking for?",
+                        value: "Don't see what you're looking for?",
+                        subline: "Click to see more results for " + request.term,
+                        url: "https://censusreporter.org" //TODO
+                    });
+
+                    response(result);
+                }
+            });
+        },
+        select: function (event, ui) {
+            window.location = ui.item.url;
+        },
+        minLength: 2
+    }).autocomplete("instance")._renderItem = function(ul, item) {
+        // Format autocomplete dropdown.
+        return $("<li>").append("<div class='autocomplete-label'>" + item.label
+            + "</div><div class='autocomplete-subline'>"
+            + item.subline + "</div>").appendTo(ul);
+    };
+
+    // Constrain autocomplete box width
+    // http://stackoverflow.com/questions/5643767/jquery-ui-autocomplete-width-not-set-correctly
+    jQuery.ui.autocomplete.prototype._resizeMenu = function () {
+        var ul = this.menu.element;
+        ul.outerWidth(this.element.outerWidth());
+    };
+});
