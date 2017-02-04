6a900ec717ca04b9a3bba659a5fd7ae43db8fd21
JD Godchaux <jd@nijel.org>
2017-02-03 19:51:36 -0500

    removed all non-Michigain polygons from profile maps

--- census/static/js/address.search.js
+++ census/static/js/address.search.js
@@ -50,16 +50,6 @@ function updateLocation(lat, lng, label) {
 
 function processGeocoderResults(response) {
     var results = response.results;
-    console.log(results);
-    // var results = response.features;
-    // results = _.filter(results, function(item) {
-    //     return item.id.indexOf('address.') == 0;
-    // });
-    // results = _.map(results, function(item) {
-    //     // Gets rid of "United States" at the end of the address
-    //     item.place_name = item.place_name.replace(", United States", "");
-    //     return item;
-    // });
     return results;
 }
 
