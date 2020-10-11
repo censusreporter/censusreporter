9e2bdb43b3d4c01b89e2edcc0beb6932568438fa
Ian Dees <ian.dees@gmail.com>
2020-10-11 16:33:53 -0500

    Switch to Redis caching

--- censusreporter/config/prod/settings.py
+++ censusreporter/config/prod/settings.py
@@ -1,4 +1,5 @@
 from config.base.settings import *
+import os
 
 DEBUG = False
 ROOT_URLCONF = 'config.prod.urls'
@@ -8,26 +9,11 @@ WSGI_APPLICATION = "config.prod.wsgi.application"
 ALLOWED_HOSTS = [
     'censusreporter.org',
     'www.censusreporter.org',
-    '.compute-1.amazonaws.com',  # allows viewing of instances directly
-    'cr-prod-409865157.us-east-1.elb.amazonaws.com',  # from the load balancer
 ]
-
-# From https://forums.aws.amazon.com/thread.jspa?messageID=423533:
-# "The Elastic Load Balancer HTTP health check will use the instance's internal IP."
-# From https://dryan.com/articles/elb-django-allowed-hosts/
-import requests
-EC2_PRIVATE_IP = None
-try:
-    EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
-except requests.exceptions.RequestException:
-    pass
-
-if EC2_PRIVATE_IP:
-    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': 'localhost:11211',
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': [os.environ.get('REDIS_URL')],
     }
 }
--- censusreporter/config/prod/wsgi.py
+++ censusreporter/config/prod/wsgi.py
@@ -2,8 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
-import newrelic.agent
-newrelic.agent.initialize('newrelic.ini')
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.prod.settings")
 application = Sentry(get_wsgi_application())
